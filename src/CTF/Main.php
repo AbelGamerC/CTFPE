6d2924e93e1777077858df21ec1df1a559275946
SavionLegendZzz <saviondeavault@gmail.com>
2015-12-15 18:52:23 -0500

    Create Main.php

---
+++ src/CTF/Main.php
@@ -0,0 +1,37 @@
+<?php
+namespace CTF;
+
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\utils\Config;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageByChildEntityEvent;
+
+
+class Main extends PluginBase implements Listener
+{
+    private $hg;
+public $players = array();
+public $pl = array();
+public $compass = array();
+
+    public function onLoad(){
+       @mkdir($this->getDataFolder());
+        $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(/*todo*/));
+		$this->config->save();
+        $this->getServer()->getLogger()->info("[CTF]Plugin is loading!");
+     }
+}
+    
+    public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new gameTask($this),20);
+        $this->getServer()->getLogger()->info("[CTF]Plugin has been enabled!");
+											  
+} 
