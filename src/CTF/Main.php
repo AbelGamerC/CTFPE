fa659412b9aea03a0f1fd8c0feea6f01b742f60d
SavionLegendZzz <saviondeavault@gmail.com>
2015-12-15 19:06:55 -0500

    Update Main.php

--- src/CTF/Main.php
+++ src/CTF/Main.php
@@ -13,6 +13,8 @@ use pocketmine\utils\Config;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageByChildEntityEvent;
 
+use pocketmine\event\player\PlayerJoinEvent;
+
 
 class Main extends PluginBase implements Listener
 {
@@ -29,5 +31,43 @@ class Main extends PluginBase implements Listener
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new gameTask($this),20);
         $this->getServer()->getLogger()->info("[CTF]Plugin has been enabled!");
+		$this->redPlayers = []; //max amount is 5
+		$this->bluePlayers = [];
 											  
 } 
+
+
+public function addRedPlayer(Player $p){
+	$this->redPlayers[$p->getName()] = array("red" => $p->getName());
+}
+
+public function addBluePlayer(Player $p){
+	$this->bluePlayers[$p->getName()] = array("blue" => $p->getName());
+}
+
+public function pickTeam(Player $p){
+if(count($this->redPlayers) === 5 && count($this->bluePlayers) === 5){
+	$p->sendMessage("All teams are full! Removing you from the server! in 2 seconds!");
+	$task = new kickTask($this, $this);
+	$this->getServer()->getScheduler()->scheduleDelayedTask($task, 20*2);
+	return;
+} 
+
+if(count($this->bluePlayers) < count($this->redPlayers)){
+	$this->addBluePlayer($p);
+	//tp to blue spot
+}else{
+	$this->addRedPlayer($p);
+	//tp to red spot
+}
+
+}
+
+
+public function onJoin(PlayerJoinEvent $ev){
+	$p = $ev->getPlayer();
+	$this->pickTeam($p);
+	
+}
+
+}
